65453f2507317c74e9dd9b3a7ede4a82a2f2d267
Dr Xam <samuelhappiness360@gmail.com>
2025-03-15 01:43:16 +0100

    server

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ import jsonServer from "json-server";
 import auth from "json-server-auth";
 
 const server = express();
+const port = process.env.PORT || 8080;
+
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', '*')
-    next()
-})
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 
 const router = jsonServer.router('./data/db.json');
 server.use('/api', router);
-server.db = router.db
+server.db = router.db;
 
-const middlewares = jsonServer.defaults()
+const middlewares = jsonServer.defaults();
 const rules = auth.rewriter({
     products: 444,
     featured_products: 444,
@@ -21,9 +24,10 @@ const rules = auth.rewriter({
     users: 600
 });
 
-server.use(rules)
-server.use(auth)
-server.use(middlewares)
+server.use(rules);
+server.use(auth);
+server.use(middlewares);
 
-
-server.listen(8080);
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
